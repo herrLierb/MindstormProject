4c308ac19bd94b6b2574f2953f6dc525f7efc480
Felix Dross <felixdross@gmail.com>
2011-12-15 13:40:56 +0100

    Alfred tuning

--- Embedded_Lejos/src/alfred/Alfred.java
+++ Embedded_Lejos/src/alfred/Alfred.java
@@ -10,11 +10,13 @@ public class Alfred {
 
 	// Set parameters here: --------------------------------------------
 
-	private double Kp = 0.35; //0.3, 0.21, 0.29;
-	private double Ki = 0.65 * Kp; // 0.0;
-	private double Kd = 4.5 * Kp; //
+	private double Kc = 0.33;
+	private double dt = 0.004198;
+	private double Kp = 0.33; // 0.6 * Kc; // 0.32; //0.3, 0.21, 0.29; // 0.35
+	private double Ki = 0.65 * Kp; // 2 * Kp * dt; // 0.65 * Kp; // 0.0; // 0.5
+	private double Kd = 4.5 * Kp; // Kp * 1/(8 * dt); // 4.5 * Kp; //
 
-	private double speedFactor = 0.6;
+	private double speedFactor = 0.55; // 0.5
 	private double optimumShift = 0.7;
 
 	private double maxIntegral = 1.0;
@@ -47,6 +49,10 @@ public class Alfred {
 	public void run() {
 		double currentError, lastError = 0.0;
 		double u;
+		
+		long timeStamp = System.currentTimeMillis();
+		int count = 0;
+		
 		while (true) {
 			currentError = this.getCurrentPercentageError();
 			if (errorIntegral > 0) {
@@ -57,24 +63,36 @@ public class Alfred {
 						this.errorIntegral + currentError);
 			}
 
-			this.errorDifferential = currentError - lastError;
+			this.errorDifferential = (currentError - lastError); // dt;
 
 			u = (Kp * currentError) + (Ki * this.errorIntegral)
 					+ (Kd * this.errorDifferential);
 			if (u < -1.0) {
 				u = -1.0;
-				System.out.println("----> too low");
+				// System.out.println("----> too low");
 			} else if (u > 1.0) {
-				System.out.println("----> too high");
+				// System.out.println("----> too high");
 				u = 1.0;
 			}
 			
-			System.out.println(u);
+			if(u > -0.05 && u < 0.05) {
+				u = 0;
+			}
+			
+			// System.out.println(u);
 
 			myPilot.steer((int) (u * 200.0));
 
-
 			lastError = currentError;
+			
+			if(count < 10001) {
+				count ++;
+			}
+				
+			if(count == 10000) {
+				System.out.println(System.currentTimeMillis() - timeStamp);
+			} 
+			
 		}
 	}
 
@@ -98,7 +116,7 @@ public class Alfred {
 		this.highReadInValue = Math.max(this.highReadInValue, currentValue);
 		this.lowReadInValue = Math.min(this.lowReadInValue, currentValue);
 		double diff = this.highReadInValue - this.lowReadInValue;
-		return (int) (this.lowReadInValue + optimumShift*diff);
+		return (int) (this.lowReadInValue + optimumShift * diff);
 	}
 
 	public void setHighReadInValue(int value) {
